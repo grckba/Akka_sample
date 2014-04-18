4181d98b5193b7f7fba979613e075c80bc8a2d5d
Grace Cabana <gmcabanah@unal.edu.co>
2014-04-18 13:09:25 -0500

    En los paquetes se encuentran:  - <default package>: Ejemplo profe  - sample.hello: Ejemplo de la documentación akka

---
+++ src/Akkatutorial.java
@@ -0,0 +1,21 @@
+import akka.actor.*;
+import akka.actor.Props;
+
+public class Akkatutorial {
+
+    /**
+     *
+     * @param args the command line arguments
+     *
+     */
+    public static void main(String[] args) {
+        ActorSystem system = ActorSystem.create("Hola");
+        ActorRef helloActor = system.actorOf(Props.create(HelloActor.class), "hola");
+        
+        helloActor.tell("hello", null);
+        helloActor.tell("chao", null);
+
+        system.shutdown();
+        system.awaitTermination();//JOIN
+    }
+}
